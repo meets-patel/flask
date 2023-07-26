1e96b4cd7e1f388f99e158a6f4dff51a6ccd438a
Meet Patel <meet@gmail.com>
2023-07-26 15:27:46 +0530

    new add

--- server.py
+++ server.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-  return "Hello DITISS!!"
+  return "Hello DMC, DBDA, PRECAT!!"
 
 app.run(host="0.0.0.0",port=4010, debug=True)
