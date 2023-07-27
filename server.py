6d42c144750b2da19235bcc335494a52fd0b8327
Meet Patel <meet@gmail.com>
2023-07-27 08:58:41 +0530

    initial commit

--- server.py
+++ server.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-  return "Radhe Radhe"
+  return "<h1>Welcome to flask app v1</h1>"
 
 app.run(host="0.0.0.0",port=4010, debug=True)
